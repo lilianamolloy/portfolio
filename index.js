23c11b9b33bdc35b8bbcc6efd8e6fd2ff7a9fdfd
Lili Molloy <liliana.molloy@gmail.com>
2018-11-24 16:31:50 +1100

    added carousel for portfolio page

--- index.js
+++ index.js
@@ -45,3 +45,91 @@ function contactAnimation(e) {
         title2.classList.remove('contact-animation')
     }
 }
+
+// Carousel
+
+let bannerStatus = 1
+const bannerTimer = 4000
+
+window.onload = function() {
+    bannerLoop()
+}
+
+let startBannerLoop = setInterval(function(){
+    bannerLoop()
+}, bannerTimer)
+
+document.querySelector('#main-banner').onmouseenter = function() {
+    clearInterval(startBannerLoop)
+}
+
+document.querySelector('#main-banner').addEventListener('mouseleave', function() {
+    startBannerLoop = setInterval(function(){
+        bannerLoop()
+    }, bannerTimer)
+})
+
+document.querySelector('#imgbanbtn-prev').addEventListener('click', function() {
+    if (bannerStatus === 1) {
+        bannerStatus = 2
+    }
+    else if (bannerStatus === 2) {
+        bannerStatus = 3
+    }
+    else if (bannerStatus === 3) {
+        bannerStatus = 1
+    }
+    bannerLoop()
+})
+
+document.querySelector('#imgbanbtn-nxt').addEventListener('click', function() {
+    bannerLoop()
+})
+
+function bannerLoop() {
+    if (bannerStatus === 1) {
+        document.querySelector('#imgban2').style.opacity = '0'
+        setTimeout(function(){
+            document.querySelector('#imgban1').style.right = '0px'
+            document.querySelector('#imgban1').style.zIndex = '-100%'
+            document.querySelector('#imgban2').style.right = '-1000px'
+            document.querySelector('#imgban2').style.zIndex = '1500'
+            document.querySelector('#imgban3').style.right = '200%'
+            document.querySelector('#imgban3').style.zIndex = '500'
+        }, 500)
+        setTimeout(function(){
+            document.querySelector('#imgban2').style.opacity = '1'
+        }, 1000)
+        bannerStatus = 2
+    }
+    else if (bannerStatus === 2) {
+        document.querySelector('#imgban3').style.opacity = '0'
+        setTimeout(function(){
+            document.querySelector('#imgban2').style.right = '0px'
+            document.querySelector('#imgban2').style.zIndex = '1000'
+            document.querySelector('#imgban3').style.right = '-100%'
+            document.querySelector('#imgban3').style.zIndex = '1500'
+            document.querySelector('#imgban1').style.right = '200%'
+            document.querySelector('#imgban1').style.zIndex = '500'
+        }, 500)
+        setTimeout(function(){
+            document.querySelector('#imgban3').style.opacity = '1'
+        }, 1000)
+        bannerStatus = 3
+    }
+    else if (bannerStatus === 3) {
+        document.querySelector('#imgban1').style.opacity = '0'
+        setTimeout(function(){
+            document.querySelector('#imgban3').style.right = '0px'
+            document.querySelector('#imgban3').style.zIndex = '1000'
+            document.querySelector('#imgban1').style.right = '-100%'
+            document.querySelector('#imgban1').style.zIndex = '1500'
+            document.querySelector('#imgban2').style.right = '200%'
+            document.querySelector('#imgban2').style.zIndex = '500'
+        }, 500)
+        setTimeout(function(){
+            document.querySelector('#imgban1').style.opacity = '1'
+        }, 1000)
+        bannerStatus = 1
+    }
+}
